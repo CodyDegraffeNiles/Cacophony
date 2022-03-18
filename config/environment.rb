f49cc26dc78b73e34d8a5ca8e8dd3e4c24448947
Cody Degraffe-Niles <codypdniles@gmail.com>
2022-03-18 17:32:31 -0400

    Continue Setup

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Allows redux state to follow javascript convention
+
+Jbuilder.key_format camelize: :lower
+
