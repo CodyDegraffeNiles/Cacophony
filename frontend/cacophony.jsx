273eb03387643efaf35b2ba6e0e27cc6b543d397
Cody Degraffe-Niles <codypdniles@gmail.com>
2022-03-19 15:30:05 -0400

    Add small changes before merge

--- frontend/cacophony.jsx
+++ frontend/cacophony.jsx
@@ -4,14 +4,26 @@ import configureStore from "./store/store";
 import Root from './components/root';
 import {createUser, createSession, deleteSession} from "./util/session_api_util";
 
-
-
-document.addEventListener(('DOMContentLoaded'), () => {
-  const root = document.getElementById('root')
-  const store = configureStore()
-   // TESTING SUITE!
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+  let store;
+  if (window.currentUser) {
+  const preloadedState = {
+    entities: {
+      users: { [window.currentUser.id]: window.currentUser }
+    },
+    session: {id: window.currentUser.id }
+  };
+  store = configureStore(preloadedState);
+  delete window.currentUser;
+} else {
+  store = configureStore();
+}
+  // Window commands - Just for TESTING!
+  window.createUser = createUser;
+  window.createSession = createSession;
+  window.deleteSession = deleteSession;
   window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  // TESTING SUITE!
-  ReactDOM.render(<Root store ={store}/>, root)
-})
+  // Testing only
+  ReactDOM.render(<Root store ={store}/>, root);
+});
