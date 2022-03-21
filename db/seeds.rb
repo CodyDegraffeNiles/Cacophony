29178892cf4ca4e178abb5f24ef7e274dc62b929
Cody Degraffe-Niles <codypdniles@gmail.com>
2022-03-21 17:59:02 -0400

    Change seed data to work

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-  demoUser = User.create({
+  demoUser = User.create(
       email: "KoalaDemo@caveman.com",
       username: "DemoUser",
-      password: "DropBear"
-  })
+      password: "DropBear")
 
-  testServer = Server.create({
+  testServer = Server.create(
     name: "Sleepy Koalas",
-    owner_id: 12, 
-    public: false
-})
+    owner_id: 1, 
+    public: false)
