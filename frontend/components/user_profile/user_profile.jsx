e55c6c0f8f1cce5189b88adb77788f9d7f263a4c
Cody Degraffe-Niles <codypdniles@gmail.com>
2022-03-21 17:48:00 -0400

    modify demo user restrictions

--- frontend/components/user_profile/user_profile.jsx
+++ frontend/components/user_profile/user_profile.jsx
@@ -11,7 +11,7 @@ class UserProfile extends React.Component{
   handleSubmit(e){
     e.preventDefault();
     e.stopPropagation();
-    if(this.props.user.id === 12) {return}
+    if(this.props.user.email === "KoalaDemo@caveman.com") {return}
     this.props.update(this.state);
   }
 
@@ -23,7 +23,8 @@ class UserProfile extends React.Component{
 
 
   render(){
-    const deleteButton = this.props.user.id === 12 ? <button id ="delete-user"> 
+   // Modify delete button to do nothing if demo-user is loged in"
+    const deleteButton = this.props.user.email === "KoalaDemo@caveman.com" ? <button id ="delete-user"> 
       Delete Account(Disabled)</button> : 
     <button id ="delete-user" onClick={() => this.props.delete(this.props.user.id)}>Delete Account</button>
   
