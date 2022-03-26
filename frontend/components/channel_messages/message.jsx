e2e7ebdfdde87365f909409115a98cff5adc07f1
Cody Degraffe-Niles <codypdniles@gmail.com>
2022-03-26 01:06:24 -0400

    Increasing styling

--- frontend/components/channel_messages/message.jsx
+++ frontend/components/channel_messages/message.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
 
+// Make Class Component so I can modify the created At
 
 const Message = (props) => {
   return(
     <li className="server-message">
-      <p className="message-author"> {props.message.authorName} </p>
+      <p className="message-author"> {props.message.authorName} 
+      <span> {props.message.createdAt}</span>
+      </p>
       <p className="message-body">{props.message.body} </p>
     </li>
   )
