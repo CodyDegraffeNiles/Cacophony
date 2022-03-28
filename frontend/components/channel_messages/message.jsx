66fde868922e6ea8fed2d8d59b38e091c2468939
Cody Degraffe-Niles <codypdniles@gmail.com>
2022-03-28 00:47:45 -0400

    Add pencil edit icon

--- frontend/components/channel_messages/message.jsx
+++ frontend/components/channel_messages/message.jsx
@@ -5,12 +5,19 @@ import React from 'react'
 class Message extends React.Component{  
 
   render(){
+    let editIcon = this.props.currentUserId === this.props.message.authorId ? 
+    <i className="fa-solid fa-pencil fa-xs"/> : null;
+
     return(
     <li className="server-message">
       <p className="message-author"> {this.props.message.authorName} 
       <span className="message-timestamp"> {this.props.message.createdAt}</span>
       </p>
-      <p className="message-body">{this.props.message.body} </p>
+      <div className="message-body">
+      <span>{this.props.message.body} </span>
+      {editIcon}
+      </div>
+
     </li>
   )
     }
---
+++ frontend/components/channel_messages/message_container.jsx
@@ -0,0 +1,23 @@
+// import {connect} from "react-redux";
+// import Message from "./message";
+
+
+
+// const mapStateToProps = (state, ownProps) => {
+//   // Fail Safe Check
+
+// }
+
+
+// const mapDispatchToProps = (dispatch, ownProps) => {
+//   return{
+
+//   }
+// }
+
+
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Message)
+
+
+
