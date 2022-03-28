62ae6e96c9f1fe06befc3cd21de0ee0a51e151c9
Cody Degraffe-Niles <codypdniles@gmail.com>
2022-03-28 17:31:02 -0400

    small changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
-
 #Destroy all of the databases
   User.destroy_all
   Server.destroy_all
