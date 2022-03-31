0bc8c3b9a9b007eb16a60b701d226d6b55ca4c10
Cody Degraffe-Niles <codypdniles@gmail.com>
2022-03-31 04:14:37 -0400

    Built Backbone of dm adding

--- app/controllers/api/dm_servers_controller.rb
+++ app/controllers/api/dm_servers_controller.rb
@@ -2,15 +2,16 @@ class Api::DmServersController < ApplicationController
 
   def index
     @dm_servers = DmServer.all;
-    @current_user = userid ? current_user : false;
+    @current_user = current_user;
     @dm_servers = @current_user.dm_servers if (@current_user)
     render :index
   end
 
   def create
-    @dm_server = DmServer.new(server_params)
-    if @dmserver.save
-      render :show
+    @dm_server = DmServer.new()
+    @current_user = current_user
+    if @dm_server.save
+      render :new
     else  
       render json: @dm_server.errors.full_messages, status: 400
     end
@@ -24,13 +25,14 @@ class Api::DmServersController < ApplicationController
 
   def destroy
     @dm_server = Server.find_by(id: params[:id])
+    @current_user = current_user
     @dm_server.destroy
   end
 
   private
-  def dm_server_params
-    params.require(:dm_server).permit(:owner_id, :id)
-  end
+  # def dm_server_params
+  #   params.require(:dm_server).permit(:owner_id, :id)
+  # end
 
   def userid
     params[:user]
---
+++ app/views/api/dm_servers/new.json.jbuilder
@@ -0,0 +1,3 @@
+json.server do 
+  json.extract! @dm_server 
+end
