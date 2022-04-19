5c9bf0ab53ea9d5a2af430c37a8bff3321636197
Cody Degraffe-Niles <codypdniles@gmail.com>
2022-04-19 15:15:40 -0400

    Create second div on homepage

--- frontend/components/home_body/home_body.jsx
+++ frontend/components/home_body/home_body.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom"
 
 const HomeBody = () => {
   return(
-    <div>
+    <div id="home">
       <div id="first-div"> 
         {/* <img id="first-home-image"></img> */}
         <div id="imagine-message">
-          <h1 id="imagine-header">IMAGINE A PLACE...</h1>
+          <h2 id="imagine-header">IMAGINE A PLACE...</h2>
           <p id="imagine-text">...where you can belong to a school club, a gaming group, 
           or a worldwide art community. 
           Where just you and a handful of friends can spend time together. 
@@ -15,6 +15,17 @@ const HomeBody = () => {
           </p>
           <Link to="/signup"> Don't Just Imagine. Signup!</Link>
         </div>
+      </div>
+      <div id= "second-div">
+        <img id="second-div-image"/> 
+        <div id="second-div-message"> 
+          <h2 id="second-div-header"> Create an invite-only place where you belong</h2>
+          <p id="second-div-body">
+            Cacophony servers are organized into topic-based channels where you can 
+            collaborate, share, and just talk about your day without clogging 
+            up a group chat.
+          </p>
+        </div> 
       </div>
     </div>
   )
